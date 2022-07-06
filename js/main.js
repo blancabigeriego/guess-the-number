11007b0f726617e321298c34af017b35e708f8fe
Blanca <blancabcbt@gmail.com>
2022-07-06 10:08:54 +0200

    add some styles

--- js/main.js
+++ js/main.js
@@ -24,31 +24,33 @@ let count = 0;
 function getRandomNumber(max) { 
     return Math.ceil(Math.random() * max);
     
-} 
+}; 
+
+let random = getRandomNumber(100);
 
 function compare(){
-    const inputValue = input.value;
+    const inputValue = parseInt(input.value);
     
-    let random = getRandomNumber(100);
+    
     console.log("Mi número aleatorio es" + " " + random);
 
     if( inputValue === random){
         clueParagraph.innerHTML = "Has ganado campeona!!!";
     }
+    else if( inputValue > 100){
+        clueParagraph.innerHTML = "Tu número debe estar entre 1-100";
+     }
 
+    else if (inputValue <= 0){
+        clueParagraph.innerHTML = "Tu número debe estar entre 1-100";
+    }
     else if (inputValue > random){
         clueParagraph.innerHTML = "Tú número es demasiado alto";
     }
     else if (inputValue < random){
         clueParagraph.innerHTML = "Tu número es demasiado bajo";
     }
-     else if( inputValue > 100){
-        clueParagraph.innerHTML = "Tu número debe estar entre 1-100";
-     }
-
-    else if (inputValue < 0){
-        clueParagraph.innerHTML = "Tu número debe estar entre 1-100";
-    }
+    
 };
 
 
