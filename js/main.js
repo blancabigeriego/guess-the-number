ccb7654391f24b1aa6384142aaf78a92949ce40f
Blanca <blancabcbt@gmail.com>
2022-07-06 09:30:17 +0200

    add click count

--- js/main.js
+++ js/main.js
@@ -4,19 +4,23 @@
 // X Hacer función de numero random
 //X Hacer funcion manejadora del evento
 //X Hacer función condicional según el value del input y el random
-//Hacer función contador de prueba
+// X Hacer función contador de prueba
 //Publicar
 
 
 'use strict';
 
+//QUERY SELECTOR
 const input = document.querySelector('.js_input');
 const btn = document.querySelector('.js_btn');
 const clueParagraph = document.querySelector('.js_clue');
 const tryParagraph = document.querySelector('.js_try');
 
+//VARIABLES GLOBALES
+let count = 0;
 
 
+//FUNCIONES
 function getRandomNumber(max) { 
     return Math.ceil(Math.random() * max);
     
@@ -45,14 +49,18 @@ function compare(){
     else if (inputValue < 0){
         clueParagraph.innerHTML = "Tu número debe estar entre 1-100";
     }
-}
+};
 
 
+btn.onclick = function () {
+    count++;
+    tryParagraph.innerHTML = "Número de intentos"+ " " + count;
+};
 
+//FUNCION MANEJADORA DE EVENTO
 function handleClick(ev){
     ev.preventDefault();
     compare();
-  
 }
 
 
