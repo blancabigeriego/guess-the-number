e72c2c0af5cce00327dd2463259a99e455188d91
Blanca <blancabcbt@gmail.com>
2022-07-06 09:20:39 +0200

    add compare function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 //x Crear estructura básica HTML
 // X Traerme los elementos que necesito de HTML a JS
 // x Escuchar evento en botón
-//Hacer función de numero random
-//Hacer funcion manejadora del evento
-//Hacer función condicional según el value del input y el random
+// X Hacer función de numero random
+//X Hacer funcion manejadora del evento
+//X Hacer función condicional según el value del input y el random
 //Hacer función contador de prueba
+//Publicar
 
 
 'use strict';
@@ -17,18 +18,41 @@ const tryParagraph = document.querySelector('.js_try');
 
 
 function getRandomNumber(max) { 
-     return Math.ceil(Math.random() * max);
+    return Math.ceil(Math.random() * max);
+    
 } 
 
+function compare(){
+    const inputValue = input.value;
+    
+    let random = getRandomNumber(100);
+    console.log("Mi número aleatorio es" + " " + random);
+
+    if( inputValue === random){
+        clueParagraph.innerHTML = "Has ganado campeona!!!";
+    }
+
+    else if (inputValue > random){
+        clueParagraph.innerHTML = "Tú número es demasiado alto";
+    }
+    else if (inputValue < random){
+        clueParagraph.innerHTML = "Tu número es demasiado bajo";
+    }
+     else if( inputValue > 100){
+        clueParagraph.innerHTML = "Tu número debe estar entre 1-100";
+     }
+
+    else if (inputValue < 0){
+        clueParagraph.innerHTML = "Tu número debe estar entre 1-100";
+    }
+}
 
 
 
 function handleClick(ev){
     ev.preventDefault();
-    console.log("Mi número aleatorio es" + " " + getRandomNumber(100));
-    
-    
-
+    compare();
+  
 }
 
 
